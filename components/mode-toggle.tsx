984e097336af5921c9855fd4607c60547f126e78
Johan Gutierrez <johgutierrez22@cftsa.cl>
2024-11-14 17:48:58 -0300

    Actualizar el estilo del conmutador de modo para mejorar la visibilidad del tema

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -19,10 +19,10 @@ export function ModeToggle ()
     <Button
       size="icon"
       onClick={ toggleTheme }
-      className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-7 w-7"
+      className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-7 w-7 bg-transparent"
     >
-      <Sun className="h-6 w-6 hidden dark:block" />
-      <Moon className="h-6 w-6 block dark:hidden" />
+      <Sun className="h-6 w-6 hidden dark:block text-white" />
+      <Moon className="h-6 w-6 block dark:hidden text-black" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
