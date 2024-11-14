7a1a03832fbadf098ef88570883d69ffbe889d63
Johan Gutierrez <johgutierrez22@cftsa.cl>
2024-11-14 17:56:12 -0300

    Actualizar colores del icono del conmutador de modo para mejorar la visibilidad en el tema oscuro

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -21,8 +21,8 @@ export function ModeToggle ()
       onClick={ toggleTheme }
       className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-7 w-7 bg-transparent"
     >
-      <Sun className="h-6 w-6 hidden dark:block text-white" />
-      <Moon className="h-6 w-6 block dark:hidden text-black" />
+      <Sun className="h-6 w-6 hidden dark:block text-popover-foreground" />
+      <Moon className="h-6 w-6 block dark:hidden text-popover-foreground" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
