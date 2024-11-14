272b4d1002b86ac3745bab0d962d8b697b63e999
Johan Gutierrez <johgutierrez22@cftsa.cl>
2024-11-14 11:56:12 -0300

    Agregar menú de navegación y optimizar el conmutador de modo

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -5,38 +5,26 @@ import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import
-  {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
 
 export function ModeToggle ()
 {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+
+  const toggleTheme = () =>
+  {
+    setTheme( theme === "light" ? "dark" : "light" )
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={ () => setTheme( "light" ) }>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={ () => setTheme( "dark" ) }>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={ () => setTheme( "system" ) }>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={ toggleTheme }
+      className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground py-2 h-8 w-8 px-0"
+    >
+      <Sun className="h-6 w-6 hidden dark:block" />
+      <Moon className="h-6 w-6 block dark:hidden" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
